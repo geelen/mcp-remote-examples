b46f3f849fa0e830c1eac91696dd5bf6ad227be1
Glen Maddern <glen@cloudflare.com>
2025-03-12 14:48:07 +1100

    Slightly more generic sessionId fallback

--- 02-user-password/src/lib/MCPEntrypoint.ts
+++ 02-user-password/src/lib/MCPEntrypoint.ts
@@ -9,8 +9,9 @@ export abstract class MCPEntrypoint extends DurableObject {
 
   static Router = class extends WorkerEntrypoint<{ MCP_OBJECT: DurableObjectNamespace<MCPEntrypoint> }> {
     async fetch(request: Request) {
-      // const sessionId = c.req.query('sessionId');
-      const sessionId = this.ctx.props.userEmail // todo: types
+      const url = new URL(request.url)
+      // ctx.props can identify the user if the sessionId isn't provided
+      const sessionId = url.searchParams.get('sessionId') || JSON.stringify(this.ctx.props)
       const object = this.env.MCP_OBJECT.get(this.env.MCP_OBJECT.idFromName(sessionId))
       return object.fetch(request)
     }
