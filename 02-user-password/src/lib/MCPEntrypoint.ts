150d437fd28ad1a4c2ae5db9905aa4708d9637f5
Glen Maddern <glen@cloudflare.com>
2025-03-12 14:45:04 +1100

    Documenting an alternative API that didnt work out

--- 02-user-password/src/lib/MCPEntrypoint.ts
+++ 02-user-password/src/lib/MCPEntrypoint.ts
@@ -31,3 +31,46 @@ export abstract class MCPEntrypoint extends DurableObject {
     return new Response('Not Found', { status: 404 })
   }
 }
+
+// Alternative API sketch (couldn't make it work, server transport already closed?)
+
+//const server = new McpServer({
+//   name: 'Demo',
+//   version: '1.0.0',
+// })
+//
+// server.tool('add', { a: z.number(), b: z.number() }, async ({ a, b }) => ({
+//   content: [{ type: 'text', text: String(a + b) }],
+// }))
+//
+// // Wrap the MCP Server in a DurableObject definition
+// export const MyMCP = MCPEntrypoint(server)
+
+//export function MCPEntrypoint(server: McpServer) {
+//   return class MCPEntrypoint extends DurableObject {
+//     static Router = class extends WorkerEntrypoint<{ MCP_OBJECT: DurableObjectNamespace<MCPEntrypoint> }> {
+//       async fetch(request: Request) {
+//         // const sessionId = c.req.query('sessionId');
+//         const sessionId = this.ctx.props.userEmail // todo: types
+//         const object = this.env.MCP_OBJECT.get(this.env.MCP_OBJECT.idFromName(sessionId))
+//         return object.fetch(request)
+//       }
+//     }
+//
+//     transport = new SSEEdgeTransport('/sse/message', this.ctx.id.toString())
+//
+//     async fetch(request: Request) {
+//       const url = new URL(request.url)
+//
+//       if (url.pathname === '/sse') {
+//         await server.connect(this.transport)
+//         return addCorsHeaders(this.transport.sseResponse, request)
+//       }
+//
+//       if (url.pathname === '/sse/message') {
+//         return this.transport.handlePostMessage(request)
+//       }
+//       return new Response('Not Found', { status: 404 })
+//     }
+//   }
+// }
