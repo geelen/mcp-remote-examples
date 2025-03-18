dad4afceb3dc0e7c1b70f49aabda6eecdd958d33
Glen Maddern <glen@cloudflare.com>
2025-03-18 21:05:41 +1100

    Updated example 02 to use DurableMCP

--- 02-user-password/src/index.ts
+++ 02-user-password/src/index.ts
@@ -1,32 +1,31 @@
 import app from './routes'
 import OAuthProvider from 'workers-oauth-provider'
-import { MCPEntrypoint } from 'mcp-entrypoint'
+import { DurableMCP } from 'mcp-entrypoint'
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { z } from 'zod'
 
-export class MyMCP extends MCPEntrypoint {
-  get server() {
-    const server = new McpServer({
-      name: 'Demo',
-      version: '1.0.0',
-    })
-    server.tool('add', { a: z.number(), b: z.number() }, async ({ a, b }) => ({
+export class MyMCP extends DurableMCP {
+  server = new McpServer({
+    name: 'Demo',
+    version: '1.0.0',
+  })
+  async init() {
+    this.server.tool('add', { a: z.number(), b: z.number() }, async ({ a, b }) => ({
       content: [{ type: 'text', text: String(a + b) }],
     }))
-    // server.tool('subtract', { a: z.number(), b: z.number() }, async ({ a, b }) => ({
-    //   content: [{ type: 'text', text: String(b - a) }],
-    // }))
-    // server.tool('multiply', { a: z.number(), b: z.number() }, async ({ a, b }) => ({
-    //   content: [{ type: 'text', text: String(a * b) }],
-    // }))
-    return server
+    this.server.tool('subtract', { a: z.number(), b: z.number() }, async ({ a, b }) => ({
+      content: [{ type: 'text', text: String(b - a) }],
+    }))
+    this.server.tool('multiply', { a: z.number(), b: z.number() }, async ({ a, b }) => ({
+      content: [{ type: 'text', text: String(a * b) }],
+    }))
   }
 }
 
 // Export the OAuth handler as the default
 export default new OAuthProvider({
   apiRoute: '/sse',
-  apiHandler: MyMCP.Router,
+  apiHandler: MyMCP.mount('/sse'),
   defaultHandler: app,
   authorizeEndpoint: '/authorize',
   tokenEndpoint: '/token',
