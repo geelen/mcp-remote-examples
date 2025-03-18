e1d4c11687335b9cc76b5640afcd01a203321d5a
Glen Maddern <glen@cloudflare.com>
2025-03-18 20:23:32 +1100

    Standardising on DurableMCP

--- .vendor/mcp-entrypoint/MCPEntrypoint.ts
+++ .vendor/mcp-entrypoint/MCPEntrypoint.ts
@@ -1,67 +1,8 @@
 import { DurableObject, WorkerEntrypoint } from 'cloudflare:workers'
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { SSEEdgeTransport } from './sseEdge'
-import { addCorsHeaders } from './utils'
 import { Hono } from 'hono'
-import { cors, type CORSOptions } from 'hono/cors'
-
-export abstract class MCPEntrypoint<
-  T extends Record<string, any> = Record<string, any>,
-> extends DurableObject {
-  abstract server: McpServer
-  props!: T
-
-  static Router = class extends WorkerEntrypoint<{
-    MCP_OBJECT: DurableObjectNamespace<MCPEntrypoint>
-  }> {
-    async fetch(request: Request) {
-      console.log({
-        request: request.url,
-        method: request.method,
-        headers: Object.fromEntries(request.headers.entries()),
-      })
-      const url = new URL(request.url)
-      const sessionId = url.searchParams.get('sessionId')
-
-      if (!sessionId && url.pathname !== '/sse') {
-        return new Response(
-          'Missing sessionId. Expected POST to /sse to initiate new one',
-          { status: 400 }
-        )
-      }
-      const id = sessionId
-        ? this.env.MCP_OBJECT.idFromString(sessionId)
-        : this.env.MCP_OBJECT.newUniqueId()
-      const object = this.env.MCP_OBJECT.get(id)
-      object.init(this.ctx.props)
-      return object.fetch(request)
-    }
-  }
-  private transport!: SSEEdgeTransport
-
-  init(props: T) {
-    console.log({ init: props })
-    this.props = props
-  }
-
-  async fetch(request: Request) {
-    const url = new URL(request.url)
-
-    if (url.pathname === '/sse') {
-      this.transport = new SSEEdgeTransport(
-        '/sse/message',
-        this.ctx.id.toString()
-      )
-      await this.server.connect(this.transport)
-      return addCorsHeaders(this.transport.sseResponse, request)
-    }
-
-    if (url.pathname === '/sse/message') {
-      return this.transport.handlePostMessage(request)
-    }
-    return new Response('Not Found', { status: 404 })
-  }
-}
+import { cors } from 'hono/cors'
 
 export abstract class DurableMCP<
   T extends Record<string, any> = Record<string, any>,
@@ -74,7 +15,6 @@ export abstract class DurableMCP<
   abstract init(): Promise<void>
 
   async _init(props: T) {
-    console.log({ init: props })
     this.props = props
     if (!this.initRun) {
       this.initRun = true
@@ -82,23 +22,17 @@ export abstract class DurableMCP<
     }
   }
 
-  async fetch(request: Request) {
-    const url = new URL(request.url)
-    console.log({ [this.ctx.id.toString()]: url.pathname })
+  async onSSE(request: Request): Promise<Response> {
+    this.transport = new SSEEdgeTransport(
+      '/sse/message',
+      this.ctx.id.toString()
+    )
+    await this.server.connect(this.transport)
+    return this.transport.sseResponse
+  }
 
-    if (url.pathname === '/sse') {
-      this.transport = new SSEEdgeTransport(
-        '/sse/message',
-        this.ctx.id.toString()
-      )
-      await this.server.connect(this.transport)
-      return addCorsHeaders(this.transport.sseResponse, request)
-    }
-
-    if (url.pathname.startsWith('/sse/message')) {
-      return this.transport.handlePostMessage(request)
-    }
-    return new Response('Not Found', { status: 404 })
+  async onMessage(request: Request): Promise<Response> {
+    return this.transport.handlePostMessage(request)
   }
 
   static mount(
@@ -108,7 +42,7 @@ export abstract class DurableMCP<
       corsOptions,
     }: {
       binding?: string
-      corsOptions?: CORSOptions
+      corsOptions?: Parameters<typeof cors>[0]
     } = {}
   ) {
     const router = new Hono<{
@@ -116,22 +50,16 @@ export abstract class DurableMCP<
     }>()
 
     router.get(path, cors(corsOptions), async (c) => {
-      console.log('GET SSE')
-      console.log(c.req.url)
       const namespace = c.env[binding]
       const object = namespace.get(namespace.newUniqueId())
       // @ts-ignore
-      await object._init(c.executionCtx.props)
-      return await object.fetch(c.req.raw)
+      object._init(c.executionCtx.props)
+      return await object.onSSE(c.req.raw) as unknown as Response
     })
 
     router.post(path + '/message', cors(corsOptions), async (c) => {
-      console.log('POST MESSAGE')
-      console.log(c.req.url)
       const namespace = c.env[binding]
       const sessionId = c.req.query('sessionId')
-      console.log({ sessionId })
-
       if (!sessionId) {
         return new Response(
           'Missing sessionId. Expected POST to /sse to initiate new one',
@@ -139,72 +67,9 @@ export abstract class DurableMCP<
         )
       }
       const object = namespace.get(namespace.idFromString(sessionId))
-      return await object.fetch(c.req.raw)
+      return await object.onMessage(c.req.raw) as unknown as Response
     })
 
     return router
   }
-}
-
-export const WorkersMCP = {
-  mountStateful({
-    binding = 'MCP_OBJECT',
-    route,
-    server,
-  }: {
-    server: McpServer
-    route: string
-    binding?: string
-  }) {
-    class Router extends WorkerEntrypoint<{
-      [binding]: DurableObjectNamespace<MCPEntrypoint>
-    }> {
-      async fetch(request: Request) {
-        console.log({
-          request: request.url,
-          method: request.method,
-          headers: Object.fromEntries(request.headers.entries()),
-        })
-        const url = new URL(request.url)
-        const sessionId = url.searchParams.get('sessionId')
-
-        if (!sessionId && url.pathname !== '/sse') {
-          return new Response(
-            'Missing sessionId. Expected POST to /sse to initiate new one',
-            { status: 400 }
-          )
-        }
-        const id = sessionId
-          ? this.env[binding].idFromString(sessionId)
-          : this.env[binding].newUniqueId()
-        const object = this.env[binding].get(id)
-        object.init(this.ctx.props)
-        return object.fetch(request)
-      }
-    }
-
-    class Session extends DurableObject {
-      private transport!: SSEEdgeTransport
-
-      async fetch(request: Request) {
-        const url = new URL(request.url)
-
-        if (url.pathname === route) {
-          this.transport = new SSEEdgeTransport(
-            '/sse/message',
-            this.ctx.id.toString()
-          )
-          await server.connect(this.transport)
-          return addCorsHeaders(this.transport.sseResponse, request)
-        }
-
-        if (url.pathname === '/sse/message') {
-          return this.transport.handlePostMessage(request)
-        }
-        return new Response('Not Found', { status: 404 })
-      }
-    }
-
-    return { Router, Session }
-  },
 }
